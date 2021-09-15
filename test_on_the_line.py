23211dd374c11055d26e2a8e439486c8d43ee334
Stevan Kriss <stevan.kriss@duke.edu>
2021-09-15 02:07:15 -0400

    intercept calculation test works and passes

--- test_on_the_line.py
+++ test_on_the_line.py
@@ -17,3 +17,11 @@ def test_slope_calc(point1, point2, exp_slope):
     from on_the_line import slope_calc
     slope = slope_calc(point1, point2)
     assert slope == exp_slope
+    
+# unit test for intecept calculation
+@pytest.mark.parametrize("point1, point2, slope, exp_intercept", [
+    ((1, 2), (5, 8), 1.5, 0.5)])
+def test_intercept_calc(point1, point2, slope, exp_intercept):
+    from on_the_line import intercept_calc
+    intercept = intercept_calc(point1, point2, slope)
+    assert intercept == exp_intercept
