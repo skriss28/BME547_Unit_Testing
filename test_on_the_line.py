2c72bad9f9a8cb80e4ad8c04fb69bec52c6aa2ed
Stevan Kriss <stevan.kriss@duke.edu>
2021-09-15 02:03:50 -0400

    slope calculation test works and passes

--- test_on_the_line.py
+++ test_on_the_line.py
@@ -7,3 +7,13 @@ def test_on_the_line_calc(point1, point2, x, exp_y):
     from on_the_line import on_the_line_calc
     y = on_the_line_calc(point1, point2, x)
     assert y == exp_y
+
+# modular unit testing to match the broken down into steps function
+
+# unit test for slope calculation
+@pytest.mark.parametrize("point1, point2, exp_slope", [
+    ((1, 2), (5, 8), 1.5)])
+def test_slope_calc(point1, point2, exp_slope):
+    from on_the_line import slope_calc
+    slope = slope_calc(point1, point2)
+    assert slope == exp_slope
